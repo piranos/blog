1d2fbf051883fdce8d71ade20f34928926669c8f
davideckhaus <david.eckhaus@gmail.com>
2019-11-03 15:06:17 +0100

    add reaction test

---
+++ tests/blog/BlogTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Blog;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BlogTest extends WebTestCase
+{
+    /**
+     * Test creation of new blog posts
+     */
+    public function testCreatePost()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/post/new');
+        $form = $crawler->selectButton('Publish')->form();
+        // set some values
+        $form['post[title]'] = 'test post';
+        $form['post[message]'] = 'Hey there!';
+
+        // submit the form
+        $crawler = $client->submit($form);
+
+        $crawler = $client->request('GET', '/blog');
+        $this->assertGreaterThan(0, $crawler->filter('h2:contains("test post")')->count());
+
+        $link = $crawler
+            ->filter('.blog__post:contains("test post") a')
+            ->eq(1)
+            ->link();
+
+        $crawler = $client->click($link);
+        $this->assertGreaterThan(0, $crawler->filter('div:contains("Hey there!")')->count());
+    }
+
+    /**
+     * test creation of post reactions
+     */
+    public function testCreateReaction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/blog/1');
+
+        $form = $crawler->selectButton('Publish')->form();
+        // set some values
+        $form['reaction[name]'] = 'test name';
+        $form['reaction[message]'] = 'Just leaving a reaction!';
+
+        // submit the form
+        $crawler = $client->submit($form);
+        $crawler = $client->request('GET', '/blog/1');
+        $this->assertGreaterThan(0, $crawler->filter('.reaction__publisher:contains("test name")')->count());
+        $this->assertGreaterThan(0, $crawler->filter('.reaction__body:contains("Just leaving a reaction!")')->count());
+    }
+}
